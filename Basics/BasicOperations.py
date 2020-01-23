71cf9e3bd531cb98426bf856d56ddeae7e4b898a
Vitor Vicente <vitor@bu.edu>
2020-01-23 18:59:18 -0500

    Assignment Operators & Special Operators

--- Basics/BasicOperations.py
+++ Basics/BasicOperations.py
@@ -14,7 +14,6 @@ print("(c + a) // b =", (c + a) // b)
 print("c ** b =", c ** b)
 print("c % b =", c % b)
 
-
 #  Boolean manipulation
 print("\nBoolean manipulation:")
 print("d = True AND e = False\n")
@@ -33,7 +32,6 @@ print("d AND e =", d and e)
 print("d OR e =", d or e)
 print("not e =", not e)
 
-
 #  Bitwise manipulation
 print("\nBitwise manipulation:")
 print("f = 10 AND g = 4\n")
@@ -47,3 +45,77 @@ print("~f =", ~f)
 print("f ^ g =", f ^ g)
 print("f >> 2 =", f >> 2)
 print("f << 2 =", f << 2)
+
+#  Assignment Operators
+print("\nBitwise manipulation:")
+print("h = 2 AND i = 4\n")
+
+h = 2
+i = 4
+
+i = h
+print("i = h | i =", i)
+
+i = 4
+i += 4
+print("i += h | i =", i)
+
+i = 4
+i -= h
+print("i -= h | i =", i)
+
+i = 4
+i *= h
+print("i *= h | i =", i)
+
+i = 4
+i /= h
+print("i /= h | i =", i)
+
+i = 4
+i %= h
+print("i %= h | i =", i)
+
+i = 4
+i **= h
+print("i **= h | i =", i)
+
+i = 4
+i //= h
+print("i //= h | i =", i)
+
+i = 4
+i &= h
+print("i &= h | i =", i)
+
+i = 4
+i ^= h
+print("i ^= h | i =", i)
+
+i = 4
+i >>= h
+print("i >>= h | i =", i)
+
+i = 4
+i <<= h
+print("i <<= h | i =", i)
+
+#  Special Operators
+print("\nSpecial manipulation:")
+print("j = 2 AND k = 2\n")
+
+j = 2
+k = 2
+
+print("j is k", j is k)
+print("j is not k", j is not k)
+
+#  Inclusion Operators
+print("\nInclusion manipulation:")
+print("l = [1,2,3] AND m = 2\n")
+
+l = [1, 2, 3]
+m = 2
+
+print("m in l", m in l)
+print("m not in l", m not in l)
