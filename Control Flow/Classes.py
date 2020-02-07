c3578005698c3383cc4884cd51624ef2cf605217
Vitor Vicente <vitor@bu.edu>
2020-02-07 18:14:03 -0500

    Class Inheritance

--- Control Flow/Classes.py
+++ Control Flow/Classes.py
@@ -12,3 +12,14 @@ p1 = Person("John", 36)
 print(p1.name)
 
 p1.intro()
+
+
+class Student(Person):
+    def __init__(self, name, age, g):
+        self.grade = g
+        super().__init__(name, age)
+
+
+p2 = Student("Tom", 19, 2022)
+
+p2.intro()
