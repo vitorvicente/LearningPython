a03680b5d0b8f09e77ab0290a1590f4545de9b79
Vitor Vicente <vitor@bu.edu>
2020-02-09 12:08:24 -0500

    Date/Time Module

---
+++ Modules/Dates.py
@@ -0,0 +1,34 @@
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+x = datetime.datetime(2020, 5, 17)
+print(x)
+
+x = datetime.datetime.now()
+
+print(x.strftime("%a"))
+print(x.strftime("%A"))
+print(x.strftime("%w"))
+print(x.strftime("%d"))
+print(x.strftime("%b"))
+print(x.strftime("%B"))
+print(x.strftime("%m"))
+print(x.strftime("%y"))
+print(x.strftime("%Y"))
+print(x.strftime("%H"))
+print(x.strftime("%I"))
+print(x.strftime("%p"))
+print(x.strftime("%M"))
+print(x.strftime("%S"))
+print(x.strftime("%f"))
+print(x.strftime("%z"))
+print(x.strftime("%Z"))
+print(x.strftime("%j"))
+print(x.strftime("%U"))
+print(x.strftime("%W"))
+print(x.strftime("%c"))
+print(x.strftime("%x"))
+print(x.strftime("%X"))
+
