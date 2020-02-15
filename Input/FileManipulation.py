aadd0e4fa84457dd74872257e7d1bc17c1588b41
Vitor Vicente <vitor@bu.edu>
2020-02-15 09:42:18 -0500

    Extra file manipulation

---
+++ Input/FileManipulation.py
@@ -0,0 +1,24 @@
+import os
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
+f.close()
+
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile3.txt", "r")
+print(f.read())
+f.close()
+
+if os.path.exists("demofile3.txt"):
+    os.remove("demofile3.txt")
+else:
+    print("The file does not exist")
